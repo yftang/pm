aaab705cffd46238c51c669af015fc7b3bfce74c
Yifei Tang <tangyifei.tang@gmail.com>
2013-11-09 20:56:45 +0800

    Improve the email regex.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
-  before_save { self.email = email.downcase }
+  before_save { email.downcase! }
 
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
 
   validates :name,  presence: true, length: { maximum: 50 }
   validates :email, presence: true,
