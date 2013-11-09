9dda952ea1898c75ac09d510e0b26c395729422f
Yifei Tang <tangyifei.tang@gmail.com>
2013-11-09 23:35:08 +0800

    Set the sign-up action

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -33,4 +33,7 @@ Pm::Application.configure do
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
+  
+  # Speed up tests by lowering bcrypt's cost funcion.
+  ActiveModel::SecurePassword.min_cost = true
 end
---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name                  "Yifei Tang"
+    email                 "yifei@tang.com"
+    password              "foobar"
+    password_confirmation "foobar"
+  end
+end
