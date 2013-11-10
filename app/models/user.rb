a08fbf10002b8fa76b2b54af12ce3f621892aaf0
Yifei Tang <tangyifei.tang@gmail.com>
2013-11-11 00:54:59 +0800

    Finish sign in

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  before_create :create_remember_token
   before_save { email.downcase! }
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
@@ -10,4 +11,17 @@ class User < ActiveRecord::Base
   validates :password, length: { minimum: 6 }
 
   has_secure_password
+  
+  def User.new_remember_token
+    SecureRandom.urlsafe_base64
+  end
+  
+  def User.encrypt(token)
+    Digest::SHA1.hexdigest(token.to_s)
+  end
+  
+  private
+    def create_remember_token
+      self.remember_token = User.encrypt(User.new_remember_token)
+    end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,6 +13,7 @@ describe User do
   it { should respond_to(:password_digest) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
+  it { should respond_to(:remember_token) }
   it { should respond_to(:authenticate)}
   
   it { should be_valid }
@@ -108,5 +109,11 @@ describe User do
     before { @user.password = @user.password_confirmation = "a" * 5 }
     
     it { should be_invalid }
+  end
+  
+  describe "remember token" do
+    before { @user.save }
+    
+    its(:remember_token) { should_not be_blank }
   end
 end
