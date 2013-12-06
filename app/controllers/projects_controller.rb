ed76bd2a1b699fa7bde586172662d1afc1f24181
Yifei Tang <tangyifei.tang@gmail.com>
2013-12-06 14:58:09 +0800

    Add project show page.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,6 +1,12 @@
 class ProjectsController < ApplicationController
-  before_action :signed_in_user, only: [:create, :destroy]
-  
+  before_action :signed_in_user, only: [:show, :create, :destroy]
+  before_action :correct_user,   only: [:show]
+
+  def show
+    @user = current_user
+    @project = Project.find(params[:id])
+  end
+
   def create
     @project = current_user.projects.create(project_params)
     if @project.save
@@ -10,13 +16,18 @@ class ProjectsController < ApplicationController
       render 'static_pages/home'
     end
   end
-  
+
   def destroy
   end
-  
+
   private
-  
+
     def project_params
       params.require(:project).permit([:acc, :start_date, :dead_line])
+    end
+
+    def correct_user
+      project = Project.find(params[:id])
+      redirect_to root_path unless project.users.include? current_user
     end
 end
