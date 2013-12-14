9b2531c6e551c8e03ef1ce1ab5bca0906139e8cb
Yifei Tang <tangyifei.tang@gmail.com>
2013-12-14 15:31:36 +0800

    Change migration file

--- db/migrate/20131213063648_add_assigner_species_status_columns_to_project.rb
+++ db/migrate/20131213063648_add_assigner_species_status_columns_to_project.rb
@@ -1,4 +1,4 @@
-class AddAssignerSpeciesStatusColumnsToUser < ActiveRecord::Migration
+class AddAssignerSpeciesStatusColumnsToProject < ActiveRecord::Migration
   def change
     add_column :projects, :assigner, :string
     add_column :projects, :species,  :string
