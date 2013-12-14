c17fb7eaba867235935919cbaf4f4995f56105c6
Yifei Tang <tangyifei.tang@gmail.com>
2013-12-14 16:14:19 +0800

    Precompile assets

--- config/application.rb
+++ config/application.rb
@@ -24,7 +24,7 @@ module Pm
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-    
-    config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
+
+    config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif *.js *.css)
   end
 end
